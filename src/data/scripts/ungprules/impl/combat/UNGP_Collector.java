5b1500daf23bd49f81fe7acc105c8bc9c1bb6a0c
TruthOriginem <originem9527@gmail.com>
2021-10-26 18:47:18 +0800

    Hotfix

--- src/data/scripts/ungprules/impl/combat/UNGP_Collector.java
+++ src/data/scripts/ungprules/impl/combat/UNGP_Collector.java
@@ -52,24 +52,26 @@ public class UNGP_Collector extends UNGP_BaseRuleEffect implements UNGP_CombatIn
         @Override
         public void reportDamageApplied(Object source, CombatEntityAPI target, ApplyDamageResultAPI result) {
             if (hiddenCoolDown > 0f) return;
-            if (source instanceof ShipAPI && target instanceof ShipAPI) {
-                ShipAPI ship = (ShipAPI) source;
+            if (source instanceof ShipAPI && target instanceof ShipAPI && source != target) {
+                ShipAPI sourceShip = (ShipAPI) source;
                 ShipAPI targetShip = (ShipAPI) target;
-                final CombatEngineAPI engine = Global.getCombatEngine();
-                if (targetShip.isAlive() && !targetShip.isFighter() && result.getDamageToHull() > 0) {
-                    if (ship.isFighter()) {
-                        if (ship.getWing() != null) {
-                            if (ship.getWing().getSourceShip() != engine.getPlayerShip()) {
-                                return;
-                            }
+                CombatEngineAPI engine = Global.getCombatEngine();
+                // 目标需要存活，不能是战机，不能是相同所属，并且本次对结构伤害大于0
+                if (targetShip.isAlive() && !targetShip.isFighter() && sourceShip.getOwner() != targetShip.getOwner() &&
+                        result.getDamageToHull() > 0) {
+                    ShipAPI playerShip = engine.getPlayerShip();
+                    // 如果源头来自战机，看战机源头是否是玩家正在操控的船
+                    if (sourceShip.isFighter()) {
+                        if (sourceShip.getWing() != null) {
+                            if (sourceShip.getWing().getSourceShip() != playerShip) return;
                         } else {
                             return;
                         }
-                    } else {
-                        if (ship != engine.getPlayerShip()) {
-                            return;
-                        }
+                    } else if (sourceShip != playerShip) {
+                        // 如果源头不是玩家操控战舰，直接返回
+                        return;
                     }
+                    // 目标结构低于结构值时
                     if (target.getHullLevel() < COLLECT_HP_THRESHOLD) {
                         final ArmorGridAPI armorGrid = targetShip.getArmorGrid();
                         Vector2f damagePoint;
@@ -81,18 +83,16 @@ public class UNGP_Collector extends UNGP_BaseRuleEffect implements UNGP_CombatIn
                             final int[] cellAtLocation = armorGrid.getCellAtLocation(damagePoint);
                             if (cellAtLocation != null) {
                                 final MutableShipStatsAPI stats = targetShip.getMutableStats();
-                                // 取消装甲减伤
-                                stats.getMaxArmorDamageReduction().modifyMult(DEBUFF_ID, 0f);
+                                // 取消装甲最小减伤
                                 stats.getMinArmorFraction().modifyMult(DEBUFF_ID, 0f);
-                                // 设置中心装甲为0
-                                armorGrid.setArmorValue(cellAtLocation[0], cellAtLocation[1], 0f);
+                                // 设置中心区域装甲为0
+                                clearArmor(cellAtLocation[0], cellAtLocation[1], armorGrid);
                                 final float damage = target.getMaxHitpoints() * COLLECT_HP_THRESHOLD + 999f;
                                 targetShip.setHitpoints(1f);
                                 engine.applyDamage(target,
                                                    targetLocation,
                                                    damage, DamageType.OTHER,
-                                                   0f, true, false, ship, true);
-                                stats.getMaxArmorDamageReduction().unmodify(DEBUFF_ID);
+                                                   0f, true, false, sourceShip, true);
                                 stats.getMinArmorFraction().unmodify(DEBUFF_ID);
 
                                 hiddenCoolDown = 0.15f;
@@ -120,6 +120,20 @@ public class UNGP_Collector extends UNGP_BaseRuleEffect implements UNGP_CombatIn
         public void advance(float amount) {
             if (hiddenCoolDown > 0f) {
                 hiddenCoolDown -= amount;
+            }
+        }
+
+        private void clearArmor(int x, int y, ArmorGridAPI grid) {
+            int gridWidth = grid.getGrid().length;
+            int gridHeight = grid.getGrid()[0].length;
+            for (int i = -2; i <= 2; i++) {
+                for (int j = -2; j <= 2; j++) {
+                    if ((i == 2 || i == -2) && (j == 2 || j == -2)) continue; // skip corners
+                    int cx = x + i;
+                    int cy = y + j;
+                    if (cx < 0 || cx >= gridWidth || cy < 0 || cy >= gridHeight) continue;
+                    grid.setArmorValue(cx, cy, 0);
+                }
             }
         }
     }
